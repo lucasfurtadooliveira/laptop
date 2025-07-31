a29e17bc269f629fb62f2f275df9c509fcf2e12a
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2025-07-31 14:50:13 -0300

    Create initial install script with essential packages

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential default-jdk libssl-dev exuberant-ctags13 ncurses-term ack-grep silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common git vim-gtk3 curl
